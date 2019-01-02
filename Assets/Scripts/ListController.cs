78eb980440505ed9b2a3fb3aa7e4a766923d6f39
Manuel Hernández <manherna@ucm.es>
2019-01-02 16:45:10 +0100

    Planteado menú de selección

---
+++ Assets/Scripts/ListController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ListController : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject element;
+    public int numberElements;
+    public Canvas c;
+
+
+   
+
+	// Use this for initialization
+	void Start () {
+        var gridLG = gameObject.GetComponent<GridLayoutGroup> ();
+        var squareSide = Mathf.Floor(gameObject.GetComponent<RectTransform>().rect.width / (gridLG.constraintCount+1f));
+
+        Debug.Log(gameObject.GetComponent<RectTransform>().rect.width);
+
+        gridLG.cellSize = new Vector2(squareSide, squareSide);
+        gridLG.spacing = new Vector2(squareSide/10, squareSide/5);
+        
+
+        
+        var eLE = element.GetComponent<LayoutElement>();
+        
+
+
+        for (int i = 0; i < numberElements; i++)
+        {
+            var z = Instantiate(element, gameObject.transform);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Prueba.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Prueba : MonoBehaviour {
+    public GameObject button;
+    public int numberOfElements = 14;
+	// Use this for initialization
+	void Awake () {
+    
+        var cX = (Screen.width*0.99) / GetComponent<GridLayoutGroup>().constraintCount;
+        var cSize = GetComponent<GridLayoutGroup>().cellSize;
+
+        GetComponent<LayoutElement>().minWidth = (float)cX;
+        cSize.Set((float)cX, cSize.y);
+
+
+
+
+        for (var i = 0; i < numberOfElements; i++)
+        {
+            var x = Instantiate(button, gameObject.transform);
+            x.SetActive(true);
+
+        }
+	}
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
